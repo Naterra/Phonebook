27c9c11d353baaab0e1a3cc346be04fa148d56d9
Naterra <natalinyweb@hotmail.com>
2017-12-09 21:33:38 -0500

    remove node conf folder

--- config/settings.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  // we are in production
-  module.exports = require("./prod");
-} else {
-  // we are in developement
-
-  module.exports = require("./dev");
-}
